9dfd4c80a89152df3b4aee57aa6e385305443c0c
Akash <yadavakash1397@gmail.com>
2022-01-29 18:19:15 +0530

    implicit wait added

--- src/main/java/SimpleTestNGExample/BasePage.java
+++ src/main/java/SimpleTestNGExample/BasePage.java
@@ -3,10 +3,11 @@ package SimpleTestNGExample;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.concurrent.TimeUnit;
+
 public class BasePage
 {
     WebDriver driver=null;
-
 
     public void initialization()
   {
@@ -17,6 +18,7 @@ public class BasePage
         driver=new ChromeDriver();
         driver.get("https://www.google.com/");
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 
     }
   }
