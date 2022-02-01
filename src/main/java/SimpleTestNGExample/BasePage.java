7b1a931a3b4e62bc1b0f2b9e8c6653e708ee0b4d
lbalgudexor17 <laxman.balgude@xoriant.com>
2022-02-01 16:04:30 +0530

    Commit By Laxman

--- src/main/java/SimpleTestNGExample/BasePage.java
+++ src/main/java/SimpleTestNGExample/BasePage.java
@@ -1,29 +1,49 @@
 package SimpleTestNGExample;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import io.github.bonigarcia.wdm.WebDriverManager;
 import java.util.concurrent.TimeUnit;
 
 public class BasePage
 {
-    WebDriver driver=null;
+    private static WebDriver driver=null;
 
-    public void initialization()
+    public static void initialization()
   {
     if(driver==null)
     {
-        System.setProperty("webdriver.chrome.driver",
-                "E:\\AutomationProject\\ChromeDriver\\chromedriver.exe");
+       WebDriverManager.chromedriver().setup();
         driver=new ChromeDriver();
-        driver.get("https://www.google.com/");
+        driver.get(TestData.URL);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 
     }
   }
+    
+    public static boolean getLogo(WebElement logo) {
+    	boolean checklogo=logo.isDisplayed();
+    	return checklogo;
+    	
+    }
+    
+    public static WebDriver returnDriver() {
+    	return driver;
+    }
+    
+    public static String getTitle() {
+    	String title=driver.getTitle();
+    	return title;
+    }
+    
+    public static String getURL() {
+    	String URL=driver.getCurrentUrl();
+    	return URL;
+    }
 
-  public void quit()
+  public static void quit()
   {
       driver.quit();
       driver=null;
